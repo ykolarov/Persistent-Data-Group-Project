0b5916b04f4a89d0013a943fcf620f869d576411
ykolarov <kolarov01@protonmail.me>
2022-07-11 16:26:54 +0100

    Add placeholder code and instructions

--- src/main/java/com/sparta/persistentData/Model/CsvReader.java
+++ src/main/java/com/sparta/persistentData/Model/CsvReader.java
@@ -1,9 +1,17 @@
 package com.sparta.persistentData.Model;
 
 import java.io.*;
+import java.util.HashSet;
 
 public class CsvReader {
 
+    private HashSet<ValidRecord> validRecords;
+    private HashSet<ValidRecord> invalidRecords;
+
+    public CsvReader() {
+        validRecords = new HashSet<>();
+        invalidRecords = new HashSet<>();
+    }
     public String readFile(String filename) {
         String result = "";
         try (FileReader fileReader = new FileReader(filename);
@@ -12,7 +20,8 @@ public class CsvReader {
             while ((line = bufferedReader.readLine()) != null) {
                 String[] recordValues = line.split(",");
                 // TODO: check if valid and if so make new ValidRecord Object, else make new InvalidRecord Object
-                // TODO: use builder design pattern
+                // TODO: if(valid) validRecords.add(new ValidRecord(recordValues));
+                // TODO: else invalidRecords.add(new InvalidRecord(recordValues));
             }
         } catch(IOException e) {
             e.printStackTrace();
